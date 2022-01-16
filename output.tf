f732b744ad5133bdfb2d882cadb41797f34ed299
JoonShik <jshong0907@naver.com>
2022-01-17 02:10:54 +0900

    FIX: edit error

--- output.tf
+++ output.tf
@@ -6,21 +6,21 @@ output "subnet_ids" {
   value = [
     aws_subnet.public-subnet-a.id,
     aws_subnet.public-subnet-b.id,
-    aws_subent.private-subnet-a.id,
-    aws_subent.private-subnet-b.id,
+    aws_subnet.private-subnet-a.id,
+    aws_subnet.private-subnet-b.id,
   ]
 }
 
 output "public_subnet_ids" {
   value = [
-    aws_subent.public-subnet-a.id,
+    aws_subnet.public-subnet-a.id,
     aws_subnet.public-subnet-b.id,
   ]
 }
 
 output "private_subnet_ids" {
   value = [
-    aws_subent.private-subnet-a.id,
-    aws_subent.private-subnet-b.id,
+    aws_subnet.private-subnet-a.id,
+    aws_subnet.private-subnet-b.id,
   ]
 }
